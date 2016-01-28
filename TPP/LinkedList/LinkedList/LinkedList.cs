f71ada44482fb581570cd608efa9ab6ea55e93f0
Carla Fernandez <Carla Fernandez>
2016-01-29 00:00:26 +0100

    Added LinkedList project and TPP folder

---
+++ TPP/LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System.Text;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// Provides a LinkedList implementation
+    /// with Add, Remove and GetElement methods.
+    /// </summary>
+    class LinkedList
+    {
+        /// <summary>
+        /// First element of the list.
+        /// </summary>
+        public Node Head { get; private set; }
+
+        /// <summary>
+        /// Number of elements on the list.
+        /// </summary>
+        public int NumberOfElements { get; private set; }
+
+        /// <summary>
+        /// Creates a LinkedList with one element.
+        /// </summary>
+        /// <param name="firstValue">Value for the first
+        /// element of the list.</param>
+        public LinkedList(int firstValue)
+        {
+            Head = new Node(firstValue, new Node(-1, null));
+            NumberOfElements = 1;
+        }
+
+        /// <summary>
+        /// Adds a new element at the end of the list.
+        /// </summary>
+        /// <param name="value">New element to be added to the list.</param>
+        public void Add(int value)
+        {
+            Node newLast = new Node(value, null);
+            Node last;
+            last = GetNodeFromIndex(NumberOfElements - 1);
+            last.Next = newLast;
+            NumberOfElements++;
+        }
+
+        /// <summary>
+        /// Removes an element from the list.
+        /// </summary>
+        /// <param name="value">Element to be removed.</param>
+        /// <returns>Removed element or -1 
+        /// if element was not found.</returns>
+        public int Remove(int value)
+        {
+            int ret = -1;
+            Node last = GetNodeFromIndex(NumberOfElements - 1);
+            for (int i = 0; i < NumberOfElements; i++)
+            {
+                Node current = GetNodeFromIndex(i);
+                int val = current.Value;
+
+                // if we find the element
+                if (val == value)
+                {
+                    ret = val;
+                    // if it is not the last or first element
+                    if (current != Head)
+                    {
+                        Node previous = GetNodeFromIndex(i - 1);
+                        previous.Next = current.Next;
+                    }
+                    else
+                    {
+                        Head = Head.Next;
+                    }
+                    NumberOfElements--;
+                    break;
+                }
+            }
+
+            return ret;
+        }
+
+        /*public int GetElement()
+        {
+
+        }*/
+
+        /// <summary>
+        /// Returns the node at index i.
+        /// </summary>
+        /// <param name="index">Index of the node.</param>
+        /// <returns></returns>
+        private Node GetNodeFromIndex(int i)
+        {
+            Node newHead = Head;
+            for (int n = 0; n < i; n++)
+            {
+                newHead = newHead.Next;
+            }
+            return newHead;
+        }
+
+        /// <summary>
+        /// Gets an element on the list
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The element if it is on the list,
+        /// -1 if it is not on the list.</returns>
+        public int GetElement(int value)
+        {
+            for (int i = 0; i < NumberOfElements; i++)
+            {
+                Node current = GetNodeFromIndex(i);
+                int val = current.Value;
+
+                // if we find the element
+                if (val == value)
+                {
+                    return val;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns a string showing all the elements
+        /// on the list.
+        /// </summary>
+        /// <returns>List string</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            Node ptr = Head;
+            for (int i = 0; i < NumberOfElements; i++)
+            {
+                sb.Append(ptr + " ");
+                ptr = ptr.Next;
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ TPP/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList(1);
+            Console.WriteLine("Added 1: " + list.ToString());
+            Console.WriteLine("Exists 1? " + list.GetElement(1));
+            Console.WriteLine("Exists 2? " + list.GetElement(2));
+            list.Add(2);
+            Console.WriteLine("Added 2: " + list.ToString());
+            Console.WriteLine("Exists 2? " + list.GetElement(2));
+            Console.WriteLine("Exists 3? " + list.GetElement(3));
+            list.Add(3);
+            Console.WriteLine("Added 3: " + list.ToString());
+            Console.WriteLine("Exists 3? " + list.GetElement(3));
+            Console.WriteLine("Exists7? " + list.GetElement(7));
+            list.Remove(1);
+            Console.WriteLine("Removed 1: " + list.ToString());
+            Console.WriteLine("Exists 1? " + list.GetElement(1));
+            list.Remove(2);
+            Console.WriteLine("Removed 2: " + list.ToString());
+            Console.WriteLine("Exists 2? " + list.GetElement(2));
+            list.Remove(3);
+            Console.WriteLine("Removed 3: " + list.ToString());
+            Console.WriteLine("Exists 3? " + list.GetElement(3));
+        }
+    }
+}
