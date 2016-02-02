d68c1970392f7df17812a3ce0c9791fc642a554f
Carla Fernández <carla@pitando.com>
2016-02-02 10:20:54 +0100

    Separated int and polymorphic

---
+++ TPP/LinkedList_polymorphic/LinkedList/Node.cs
@@ -0,0 +1,40 @@
+﻿namespace LinkedList
+{
+    /// <summary>
+    /// Provides a LinkedListNode implementation
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Value for the node
+        /// </summary>
+        public int Value { get; protected internal set; }
+
+        /// <summary>
+        /// Pointer to the next node
+        /// </summary>
+        public Node Next { get; protected internal set; }
+
+        /// <summary>
+        /// Constructor for a linked node
+        /// </summary>
+        /// <param name="value">Value for the node</param>
+        /// <param name="next">Link to the following node</param>
+        public Node(int value, Node next)
+        {
+            this.Value = value;
+            this.Next = next;
+        }
+
+        /// <summary>
+        /// Returns the value of the Value property
+        /// </summary>
+        /// <returns>Value</returns>
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+
+    }
+}
