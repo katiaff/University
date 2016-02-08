ea157b767584982b7e30ead3135bb6f14298ca38
Carla Fernandez <carla@pitando.com>
2016-02-08 19:27:08 +0100

    Updated lab3

---
+++ TPP/Lab Uploads/i3-lab03/algorithms/IEqualityPredicate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TPP.Laboratory.ObjectOrientation.Lab03
+{
+    interface IEqualityPredicate
+    {
+        bool Compare(object p, object o);
+    }
+}
---
+++ TPP/Lab Uploads/i3-lab03/stack/Stack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace TPP.Laboratory.ObjectOrientation.Lab03 {
+
+    public class Stack {
+
+        public static void Main() {
+            Debug.Assert(false, "Message of the assertion not fulfilled ");
+        }
+
+    }
+
+}
