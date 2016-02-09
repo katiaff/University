e99db28aae3b135403816ccb6f2da0558125c9e2
unknown <uo244965@EII-L-01-09.aulas.uniovi.es>
2016-02-09 19:50:44 +0100

    Added lab2

---
+++ Algoritmia/Labs/src/lab2/Loop4.java
@@ -0,0 +1,35 @@
+package lab2;
+
+import java.util.Random;
+
+public class Loop4 {
+	public static void loop4(int n) {
+		/* algorithm O(n^4) */
+		Random rn = new Random();
+		@SuppressWarnings("unused")
+		int cont = 0;
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				for (int k = 1; k <= n; k++)
+					for (int l = 1; l <= n; l++)
+						cont += rn.nextInt();
+	}
+
+	public static void main(String arg[]) {
+		long t1, t2;
+		int nTimes = Integer.parseInt(arg[0]);
+
+		for (int n = 1; n <= 100000; n *= 2) {
+			t1 = System.currentTimeMillis();
+
+			for (int repetitions = 1; repetitions <= nTimes; repetitions++) {
+				loop4(n);
+			}
+
+			t2 = System.currentTimeMillis();
+			System.out.println("n=" + n + "**TIME=" + (t2 - t1) + "**nTimes="
+					+ nTimes);
+		} // for
+
+	}// main
+}
---
+++ Algoritmia/Labs/src/lab2/Loop5.java
@@ -0,0 +1,35 @@
+package lab2;
+
+import java.util.Random;
+
+public class Loop5 {
+	public static void loop5(int n) {
+		/* algorithm O(n^3*logn) */
+		Random rn = new Random();
+		@SuppressWarnings("unused")
+		int cont = 0;
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				for (int k = 1; k <= n; k++)
+					for (int l = 1; l <= n; l*=2)
+						cont += rn.nextInt();
+	}
+
+	public static void main(String arg[]) {
+		long t1, t2;
+		int nTimes = Integer.parseInt(arg[0]);
+
+		for (int n = 1; n <= 100000; n *= 2) {
+			t1 = System.currentTimeMillis();
+
+			for (int repetitions = 1; repetitions <= nTimes; repetitions++) {
+				loop5(n);
+			}
+
+			t2 = System.currentTimeMillis();
+			System.out.println("n=" + n + "**TIME=" + (t2 - t1) + "**nTimes="
+					+ nTimes);
+		} // for
+
+	}// main
+}
