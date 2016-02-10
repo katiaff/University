ad6df6549d517c1aa0cdd7c4b3e07930cf9e2397
Carla Fernández <carla@pitando.com>
2016-02-10 22:11:46 +0100

    TPP - Stack tests

---
+++ TPP/LinkedList_polymorphic/Stack.tests/StackTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.tests {
+    [TestClass()]
+    public class StackTests {
+        Stack<int> stack;
+
+        [TestInitialize]
+        public void CreateNewStack() {
+            stack = new Stack<int>(5);
+        }
+
+        [TestMethod()]
+        public void ThenItShouldBeEmpty() {
+            Assert.IsTrue(stack.IsEmpty);
+            Assert.AreEqual(0, stack.NumElements);
+        }
+
+        [TestMethod()]
+        public void PlayWithMaxNumElems() {
+            Assert.AreEqual(5, (int) stack.MaxNumberOfElements);
+            stack.MaxNumberOfElements = 10;
+            Assert.AreEqual(10, (int) stack.MaxNumberOfElements);
+            stack.MaxNumberOfElements = 100;
+            Assert.AreEqual(100, (int) stack.MaxNumberOfElements);
+            stack.MaxNumberOfElements = 0;
+            Assert.AreEqual(0, (int) stack.MaxNumberOfElements);
+
+        }
+
+        [TestMethod()]
+        public void StackTest1() {
+
+        }
+
+        [TestMethod()]
+        public void PushTest() {
+
+        }
+
+        [TestMethod()]
+        public void PopTest() {
+
+        }
+    }
+}
--- TPP/LinkedList_polymorphic/Stack/Stack.cs
+++ TPP/LinkedList_polymorphic/Stack/Stack.cs
@@ -6,41 +6,77 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LinkedList {
-    class Stack<T> {
-        private MyLinkedList<T> stack;
-        private uint MaxNumberOfElements { get; set; }
+    /// <summary>
+    /// Implementation of a stack
+    /// </summary>
+    /// <typeparam name="T">Type of values for the stack</typeparam>
+    public class Stack<T> {
+        /// <summary>
+        /// Stack implemented via MyLinkedList
+        /// </summary>
+        private MyLinkedList<T> Elems { get; }
 
+        /// <summary>
+        /// Maximum number of elements in the stack
+        /// </summary>
+        public uint MaxNumberOfElements { get; set; }
+
+        public int NumElements { get { return Elems.NumberOfElements; } }
+
+        /// <summary>
+        /// If the stack is empty
+        /// </summary>
         public bool IsEmpty {
-             get {
-               return stack.NumberOfElements == 0;
+            get {
+                return Elems.NumberOfElements == 0;
             }
         }
 
+        /// <summary>
+        /// If the stack is full
+        /// </summary>
         public bool IsFull {
             get {
-                return stack.NumberOfElements == this.MaxNumberOfElements;
+                return Elems.NumberOfElements == this.MaxNumberOfElements;
             }
         }
 
+        /// <summary>
+        /// Constructs an empty stack with maximum number 
+        /// of elements equal to the parameter
+        /// </summary>
+        /// <param name="maxNumberOfElements">Maximum number of elements </param>
         public Stack(uint maxNumberOfElements) {
-            stack = new MyLinkedList<T>();
+            Elems = new MyLinkedList<T>();
             this.MaxNumberOfElements = maxNumberOfElements;
         }
 
+        /// <summary>
+        /// Constructs a stack with one element
+        /// </summary>
+        /// <param name="maxNumberOfElements">>Maximum number of elements</param>
+        /// <param name="firstValue">First element</param>
         public Stack(uint maxNumberOfElements, T firstValue) {
-            stack = new MyLinkedList<T>(firstValue);
+            Elems = new MyLinkedList<T>(firstValue);
             this.MaxNumberOfElements = maxNumberOfElements;
         }
 
+        /// <summary>
+        /// Pushes an element to the stack
+        /// </summary>
+        /// <param name="element">Element to be pushed</param>
         public void Push(T element) {
-            Debug.Assert(stack.NumberOfElements != MaxNumberOfElements);
-            stack.AddBeginning(element);
+            Debug.Assert(Elems.NumberOfElements != MaxNumberOfElements);
+            Elems.AddBeginning(element);
             Debug.Assert(!IsEmpty);
         }
 
+        /// <summary>
+        /// Pops the top element from the stack
+        /// </summary>
         public void Pop() {
             Debug.Assert(!IsEmpty);
-            stack.RemoveFirst();
+            Elems.RemoveFirst();
         }
     }
 }
