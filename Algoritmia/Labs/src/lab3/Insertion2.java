8fe53cb654005b4a11a678fcbf4cc4a0dbba0052
Carla <carla@pitando.com>
2016-02-16 19:53:01 +0100

    Alg - Bubble2 changes and Insertion2 creation

--- Algoritmia/Labs/src/lab3/Insertion2.java
+++ Algoritmia/Labs/src/lab3/Insertion2.java
@@ -1,6 +1,6 @@
 package lab3;
 
-public class Bubble2 {
+public class Insertion2 {
 
 	private final static int MIN_SIZE = 10000;
 	private final static int MAX_SIZE = 1280000;
@@ -18,12 +18,12 @@ public class Bubble2 {
 			Vector.random(vector, 100);
 
 			long t1 = System.currentTimeMillis();
-			Bubble1.bubble(vector);
+			Insertion1.insertion(vector);
 			long t2 = System.currentTimeMillis();
 
 			long time = t2 - t1;
 
-			System.out.println("BUBBLE RANDOM -- size: " + vector.length
+			System.out.println("INSERTION RANDOM -- size: " + vector.length
 					+ " -- time: " + time + " ms");
 
 			vector = new int[vector.length * 2];
@@ -38,12 +38,12 @@ public class Bubble2 {
 			Vector.inverselySorted(vector);
 
 			long t1 = System.currentTimeMillis();
-			Bubble1.bubble(vector);
+			Insertion1.insertion(vector);
 			long t2 = System.currentTimeMillis();
 
 			long time = t2 - t1;
 
-			System.out.println("BUBBLE INVERSE -- size: " + vector.length
+			System.out.println("INSERTION INVERSE -- size: " + vector.length
 					+ " -- time: " + time + " ms");
 
 			vector = new int[vector.length * 2];
@@ -53,20 +53,27 @@ public class Bubble2 {
 
 	private static void timeSorted() {
 		int[] vector = new int[MIN_SIZE];
+		int nTimes = 1000000000;
 
 		while (vector.length <= MAX_SIZE) {
 			Vector.sorted(vector);
-
 			long t1 = System.currentTimeMillis();
-			Bubble1.bubble(vector);
-			long t2 = System.currentTimeMillis();
 
+			for (int i = 0; i < nTimes; i++) {
+				Insertion1.insertion(vector);
+			}
+
+			long t2 = System.currentTimeMillis();
 			long time = t2 - t1;
 
-			System.out.println("BUBBLE SORTED -- size: " + vector.length
+			System.out.println("INSERTION SORTED -- size: " + vector.length
 					+ " -- time: " + time + " ms");
 
 			vector = new int[vector.length * 2];
+			
+			if (time > 3000) {
+				nTimes /= 100;
+			}
 		}
 
 	}
