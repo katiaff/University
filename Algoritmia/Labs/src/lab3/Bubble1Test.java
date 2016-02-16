80ed52d88d66ab46e523a0a47852c06ad243ccbd
Carla <carla@pitando.com>
2016-02-16 18:41:26 +0100

    Alg - Bubble method working

---
+++ Algoritmia/Labs/src/lab3/Bubble1Test.java
@@ -0,0 +1,25 @@
+package lab3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Bubble1Test {
+
+	@Test
+	public void test() {
+		int[] elements = new int[]{0, 4, 2, 1, 3, 9, 5, 7, 8, 6};
+		
+		Bubble1.bubble(elements);
+		
+		assertCorrectOrder(elements);
+		
+	}
+
+	private void assertCorrectOrder(int[] elements) {
+		for (int i = 0; i< elements.length; i++){
+			assertEquals(i, elements[i]);
+		}
+	}
+
+}
