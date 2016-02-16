9d706a13b8e0538a9f05b7c42d0da4630d137fd2
Carla <carla@pitando.com>
2016-02-16 19:04:45 +0100

    Alg - Insertion and Bubble changes

--- Algoritmia/Labs/src/lab3/Bubble1Test.java
+++ Algoritmia/Labs/src/lab3/Bubble1Test.java
@@ -8,16 +8,16 @@ public class Bubble1Test {
 
 	@Test
 	public void test() {
-		int[] elements = new int[]{0, 4, 2, 1, 3, 9, 5, 7, 8, 6};
-		
+		int[] elements = new int[] { 0, 4, 2, 1, 3, 9, 5, 7, 8, 6 };
+
 		Bubble1.bubble(elements);
-		
+
 		assertCorrectOrder(elements);
-		
+
 	}
 
 	private void assertCorrectOrder(int[] elements) {
-		for (int i = 0; i< elements.length; i++){
+		for (int i = 0; i < elements.length; i++) {
 			assertEquals(i, elements[i]);
 		}
 	}
--- Algoritmia/Labs/src/lab3/Insertion1.java
+++ Algoritmia/Labs/src/lab3/Insertion1.java
@@ -3,37 +3,54 @@ package lab3;
 /* This program can be used to sort n elements with 
  * a "bad" algorithm (quadratic). 
  * It is the DIRECT INSERTION */
-public class Insertion1{
-	static int []v;
-	
-	public static void main (String arg [] ){
-	  int n= Integer.parseInt (arg[0]);  //size of the problem 
-	  v = new int [n] ;
-	  
-	  Vector.sorted(v);
-	  System.out.println ("VECTOR TO BE SORTED:");
-	  Vector.write(v);	
-	  insertion(v);
-	  System.out.println ("SORTED VECTOR:");
-	  Vector.write(v);
-	
-	  Vector.inverselySorted (v);
-	  System.out.println ("VECTOR TO BE SORTED:");
-	  Vector.write(v);	
-	  insertion(v);
-	  System.out.println ("SORTED VECTOR:");
-	  Vector.write(v);
-	
-	  Vector.random(v, 1000000);
-	  System.out.println ("VECTOR TO BE SORTED:");
-	  Vector.write(v);	
-	  insertion(v);
-	  System.out.println ("SORTED VECTOR:");
-	  Vector.write(v);
-	} 
-	
-	public static void insertion(int[] elements) {
-		
+public class Insertion1 {
+	static int[] v;
+
+	public static void main(String arg[]) {
+		int n = Integer.parseInt(arg[0]); // size of the problem
+		v = new int[n];
+
+		Vector.sorted(v);
+		System.out.println("VECTOR TO BE SORTED:");
+		Vector.write(v);
+		insertion(v);
+		System.out.println("SORTED VECTOR:");
+		Vector.write(v);
+
+		Vector.inverselySorted(v);
+		System.out.println("VECTOR TO BE SORTED:");
+		Vector.write(v);
+		insertion(v);
+		System.out.println("SORTED VECTOR:");
+		Vector.write(v);
+
+		Vector.random(v, 1000000);
+		System.out.println("VECTOR TO BE SORTED:");
+		Vector.write(v);
+		insertion(v);
+		System.out.println("SORTED VECTOR:");
+		Vector.write(v);
 	}
-	
-} 
+
+	public static void insertion(int[] elements) {
+		int j;
+		int pivot;
+		for (int i = 1; i < elements.length; i++) {
+			// pivot = current element
+			pivot = elements[i];
+			// j = previous index
+			j = i - 1;
+
+			// while the current element is less than the previous element
+			while (j >= 0 && pivot < elements[j]) {
+				// move the elements to the right
+				elements[j + 1] = elements[j];
+				j--;
+			}
+			// insert the element in the corresponding position
+			elements[j + 1] = pivot;
+
+		}
+	}
+
+}
