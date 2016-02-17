9c386ed210f1129e8b7de797ca2ab6839312036f
Carla <carla@pitando.com>
2016-02-17 21:56:31 +0100

    Alg - Quicksort, selection mains

---
+++ Algoritmia/Labs/src/lab3/QuicksortCentralElement2.java
@@ -0,0 +1,103 @@
+package lab3;
+
+/**
+ * Created by carla on 17/02/2016.
+ */
+public class QuicksortCentralElement2 {
+    private final static int MIN_SIZE = 512;
+    private final static int MAX_SIZE = 128000;
+    private final static int N_TIMES = 1000000;
+    private final static int TIME_LIMIT = 3000;
+
+    private static int nTimes = N_TIMES;
+
+    public static void main(String[] args) {
+        timeSorted();
+        timeInverse();
+        timeRandom();
+    }
+
+    private static void timeRandom() {
+        int[] vector = new int[MIN_SIZE];
+
+        while (vector.length <= MAX_SIZE) {
+            Vector.random(vector, 100);
+
+            long t1 = System.currentTimeMillis();
+
+            for (int i = 0; i < nTimes; i++) {
+                QuicksortCentralElement.quicksort(vector);
+            }
+            long t2 = System.currentTimeMillis();
+
+            long time = t2 - t1;
+
+            System.out.println("QUICKSORT RANDOM -- size: " + vector.length
+                    + " -- time: " + time + " ms -- nTimes: " + nTimes);
+
+            vector = new int[vector.length * 2];
+
+            if (time > TIME_LIMIT) {
+                nTimes /= 100;
+            }
+        }
+        System.out.println("\n------------------------------------------------------------------\n");
+        nTimes = N_TIMES;
+    }
+
+    private static void timeInverse() {
+        int[] vector = new int[MIN_SIZE];
+
+        while (vector.length <= MAX_SIZE) {
+            Vector.inverselySorted(vector);
+
+            long t1 = System.currentTimeMillis();
+
+            for (int i = 0; i < nTimes; i++) {
+                QuicksortCentralElement.quicksort(vector);
+            }
+
+            long t2 = System.currentTimeMillis();
+
+            long time = t2 - t1;
+
+            System.out.println("QUICKSORT INVERSE -- size: " + vector.length
+                    + " -- time: " + time + " ms -- nTimes: " + nTimes);
+
+            vector = new int[vector.length * 2];
+
+            if (time > TIME_LIMIT) {
+                nTimes /= 100;
+            }
+        }
+        System.out.println("\n------------------------------------------------------------------\n");
+        nTimes = N_TIMES;
+    }
+
+    private static void timeSorted() {
+        int[] vector = new int[MIN_SIZE];
+
+        while (vector.length <= MAX_SIZE) {
+            Vector.sorted(vector);
+            long t1 = System.currentTimeMillis();
+
+            for (int i = 0; i < nTimes; i++) {
+                QuicksortCentralElement.quicksort(vector);
+            }
+
+            long t2 = System.currentTimeMillis();
+            long time = t2 - t1;
+
+            System.out.println("QUICKSORT SORTED -- size: " + vector.length
+                    + " -- time: " + time + " ms -- nTimes: " + nTimes);
+
+            vector = new int[vector.length * 2];
+
+            if (time > TIME_LIMIT) {
+                nTimes /= 100;
+            }
+        }
+        System.out.println("\n------------------------------------------------------------------\n");
+        nTimes = N_TIMES;
+    }
+}
