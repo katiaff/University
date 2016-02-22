63413869606178038b199c5dc8a51f7dc53d6279
Carla Fernandez <carla@pitando.com>
2016-02-22 19:52:46 +0100

    TPP - Lab5 mandatory, find working

---
+++ TPP/Delegates/Delegates/ClassLibrary1/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public static class Extensions
+    {
+        public static T Find<T>(this IEnumerable<T> items, Predicate<T> pred){
+            foreach (T item in items)
+            {
+                if (pred(item)){
+                    return item;
+                }
+            }
+            throw new Exception();
+
+        }
+    }
+}
---
+++ TPP/Delegates/Delegates/Delegates.tests/PersonTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Delegates
+{
+    [TestClass]
+    public class PersonTest
+    {
+        Person[] people;
+
+        [TestInitialize]
+        public void Init()
+        {
+            people = Factory.CreatePeople();
+        }
+
+        [TestMethod]
+        public void TestFindByName()
+        {
+            Assert.AreEqual(people[0], people.Find<Person>(person => person.FirstName.Equals("María")));
+            Assert.AreEqual(people[1], people.Find<Person>(person => person.FirstName.Equals("Juan")));
+            Assert.AreEqual(people[2], people.Find<Person>(person => person.FirstName.Equals("Pepe")));
+            Assert.AreEqual(people[3], people.Find<Person>(person => person.FirstName.Equals("Luis")));
+            Assert.AreEqual(people[4], people.Find<Person>(person => person.FirstName.Equals("Carlos")));
+        }
+
+        
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestNameNotFound() 
+        {
+            Assert.AreEqual(people[0], people.Find<Person>(person => person.FirstName.Equals("María")));
+            Assert.AreEqual(people[1], people.Find<Person>(person => person.FirstName.Equals("Juan")));
+            Assert.AreEqual(people[2], people.Find<Person>(person => person.FirstName.Equals("Pepe")));
+            Assert.AreEqual(people[3], people.Find<Person>(person => person.FirstName.Equals("Luis")));
+            Assert.AreEqual(people[4], people.Find<Person>(person => person.FirstName.Equals("Carlos")));
+        }
+
+        [TestMethod]
+        public void TestFindBySurname()
+        {
+            Assert.AreEqual(people[0], people.Find<Person>(person => person.Surname.Equals("Díaz")));
+            Assert.AreEqual(people[1], people.Find<Person>(person => person.Surname.Equals("Pérez")));
+            Assert.AreEqual(people[2], people.Find<Person>(person => person.Surname.Equals("Hevia")));
+            Assert.AreEqual(people[3], people.Find<Person>(person => person.Surname.Equals("García")));
+            Assert.AreEqual(people[4], people.Find<Person>(person => person.Surname.Equals("Rodríguez")));
+        }
+
+        [TestMethod]
+        public void TestFindByID()
+        {
+            Assert.AreEqual(people[0], people.Find<Person>(person => person.IDNumber.Equals("9876384A")));
+            Assert.AreEqual(people[1], people.Find<Person>(person => person.IDNumber.Equals("103478387F")));
+            Assert.AreEqual(people[2], people.Find<Person>(person => person.IDNumber.Equals("23476293R")));
+            Assert.AreEqual(people[3], people.Find<Person>(person => person.IDNumber.Equals("4837649A")));
+            Assert.AreEqual(people[4], people.Find<Person>(person => person.IDNumber.Equals("67365498B")));
+
+        }
+    }
+}
