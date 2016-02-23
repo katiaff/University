c79a9f54cf0a7bda8cea4e35578a1428dcfbf284
Carla <carla@pitando.com>
2016-02-23 18:47:01 +0100

    Alg - Lab4 Substracion 4 correct

--- Algoritmia/Labs/src/lab4/recursive/Subtraction4.java
+++ Algoritmia/Labs/src/lab4/recursive/Subtraction4.java
@@ -7,32 +7,29 @@ package labs.course15_16.lab3dandc.recursive;
  * In this case => the stack does not overflow because 
  * long before the execution time is untreatable 
  */
-public class Subtraction4{
-	public static long rec4(int n)
-	{
-	 long cont = 0;
-	 if (n<=0) cont++;
-	 else
-	  {
-	   cont++;  //O(1)    
-	   rec3(n-1);
-	   rec3(n-1);
-	  }
-	 return cont;   
-	}
-	
-	public static void main(String arg []) 
-	{
-	 long t1,t2,cont=0;
-	 for (int n=1;n<=100;n++)
-	 {
-	  t1 = System.currentTimeMillis();
-	   
-	  cont=rec4(n);
-	 
-	  t2 = System.currentTimeMillis();
-	
-	  System.out.println ("n="+n+ "**TIME="+(t2-t1)+"**cont="+cont);
-	 }  // for
-	} // main
+public class Subtraction4 {
+    public static long rec4(int n) {
+        long cont = 0;
+        if (n <= 0) cont++;
+        else {
+            cont++;  //O(1)
+            rec4(n - 2);
+            rec4(n - 2);
+            rec4(n - 2);
+        }
+        return cont;
+    }
+
+    public static void main(String arg[]) {
+        long t1, t2, cont = 0;
+        for (int n = 1; n <= 100; n++) {
+            t1 = System.currentTimeMillis();
+
+            cont = rec4(n);
+
+            t2 = System.currentTimeMillis();
+
+            System.out.println("n=" + n + "**TIME=" + (t2 - t1) + "**cont=" + cont);
+        }  // for
+    } // main
 } //class
