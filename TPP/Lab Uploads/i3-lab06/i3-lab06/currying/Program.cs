728023035b7c4ed64eb38bb97cc57cb5d34231e0
Carla Fernandez <carla@pitando.com>
2016-02-29 18:05:13 +0100

    TPP - lab6 initial commit

---
+++ TPP/Lab Uploads/i3-lab06/i3-lab06/currying/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TPP.Laboratory.Functional.Lab06 {
+
+    class Program {
+
+        static int Addition(int a, int b) {
+            return a + b;
+        }
+
+        static void Main() {
+            Console.WriteLine(Addition(1, 2));
+        }
+
+    }
+}
