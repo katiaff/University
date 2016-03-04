7145b7e436931c8845e115cdf989b2fbe8d0de18
Carla <carla@pitando.com>
2016-03-04 12:18:09 +0100

    Alg - Quicksort full

--- Algoritmia/Labs/src/lab4/parallel/QuicksortTimes.java
+++ Algoritmia/Labs/src/lab4/parallel/QuicksortTimes.java
@@ -17,25 +17,30 @@ public class QuicksortTimes {
     private static int timeLimit;
 
     public static void main(String[] args) {
-        //int times = Integer.parseInt(args[0]);
+        int times = Integer.parseInt(args[0]);
         minSize = Integer.parseInt(args[1]);
         maxSize = Integer.parseInt(args[2]);
         timeLimit = Integer.parseInt(args[3]);
-        //nTimes = times;
-        createFiles(1);
+        nTimes = times;
+        createFiles(times);
     }
 
     private static String timeRandom() {
         ForkJoinPool pool = new ForkJoinPool();
         StringBuilder ret = new StringBuilder();
         int[] vector = new int[minSize];
-        Vector.random(vector, 100);
-        ParallelQuicksort quicksort = new ParallelQuicksort(vector, 0, vector.length - 1);
+        ParallelQuicksort quicksort;
 
         while (vector.length <= maxSize) {
 
+            vector = new int[vector.length * 2];
+            Vector.random(vector, 100);
+            quicksort = new ParallelQuicksort(vector, 0, vector.length - 1);
+
             long t1 = System.currentTimeMillis();
-            pool.invoke(quicksort);
+            for (int i = 0; i < nTimes; i++) {
+                pool.invoke(quicksort);
+            }
             long t2 = System.currentTimeMillis();
 
             long time = t2 - t1;
@@ -43,40 +48,44 @@ public class QuicksortTimes {
             ret.append("random;" + vector.length + ";" +
                     +time + ";" + nTimes + "\n");
 
-            System.out.println(vector.length + " " + time);
-
+            System.out.println("length: " + vector.length + " - time: " + time);
             if (time > timeLimit) {
-                break;
+                if (nTimes != 1) {
+                    nTimes /= 100;
+                } else {
+                    break;
+                }
             }
-            vector = new int[vector.length * 2];
-            Vector.random(vector, 100);
-            quicksort = new ParallelQuicksort(vector, 0, vector.length - 1);
 
         }
         System.out.println("\n------------------------------------------------------------------\n");
         return ret.toString();
     }
 
-/*    private static String timeInverse() {
-        int[] vector = new int[minSize];
+    private static String timeInverse() {
+        ForkJoinPool pool = new ForkJoinPool();
         StringBuilder ret = new StringBuilder();
+        int[] vector = new int[minSize];
+        ParallelQuicksort quicksort;
 
         while (vector.length <= maxSize) {
+
+            vector = new int[vector.length * 2];
             Vector.inverselySorted(vector);
+            quicksort = new ParallelQuicksort(vector, 0, vector.length - 1);
 
             long t1 = System.currentTimeMillis();
-
             for (int i = 0; i < nTimes; i++) {
-                ParallelQuicksort.quicksort(vector);
+                pool.invoke(quicksort);
             }
             long t2 = System.currentTimeMillis();
 
             long time = t2 - t1;
 
-            ret.append("inverse;" + vector.length + ";" +
+            ret.append("random;" + vector.length + ";" +
                     +time + ";" + nTimes + "\n");
 
-            vector = new int[vector.length * 2];
+            System.out.println("length: " + vector.length + " - time: " + time);
 
             if (time > timeLimit) {
                 if (nTimes != 1) {
@@ -85,31 +94,36 @@ public class QuicksortTimes {
                     break;
                 }
             }
+
         }
         System.out.println("\n------------------------------------------------------------------\n");
         return ret.toString();
     }
 
     private static String timeSorted() {
-        int[] vector = new int[minSize];
+        ForkJoinPool pool = new ForkJoinPool();
         StringBuilder ret = new StringBuilder();
+        int[] vector = new int[minSize];
+        ParallelQuicksort quicksort;
 
         while (vector.length <= maxSize) {
+
+            vector = new int[vector.length * 2];
             Vector.sorted(vector);
+            quicksort = new ParallelQuicksort(vector, 0, vector.length - 1);
 
             long t1 = System.currentTimeMillis();
-
             for (int i = 0; i < nTimes; i++) {
-                ParallelQuicksort.quicksort(vector);
+                pool.invoke(quicksort);
             }
             long t2 = System.currentTimeMillis();
 
             long time = t2 - t1;
 
-            ret.append("sorted;" + vector.length + ";" +
+            ret.append("random;" + vector.length + ";" +
                     +time + ";" + nTimes + "\n");
 
-            vector = new int[vector.length * 2];
+            System.out.println("length: " + vector.length + " - time: " + time);
 
             if (time > timeLimit) {
                 if (nTimes != 1) {
@@ -118,10 +132,11 @@ public class QuicksortTimes {
                     break;
                 }
             }
+
         }
         System.out.println("\n------------------------------------------------------------------\n");
         return ret.toString();
-    }*/
+    }
 
     private static void createFiles(int times) {
         try {
@@ -129,12 +144,12 @@ public class QuicksortTimes {
             file.append("Sorting;Size;Total time(ms);NTimes\n");
 
             System.out.println("\n----------------------QUICKSORT ALGORITHM---------------------------\n");
-//            System.out.println("Logging SORTED time");
-//            file.append(timeSorted());
-//
-//            System.out.println("Logging INVERSE time");
-//            nTimes = times;
-//            file.append(timeInverse());
+            System.out.println("Logging SORTED time");
+            file.append(timeSorted());
+
+            System.out.println("Logging INVERSE time");
+            nTimes = times;
+            file.append(timeInverse());
 
             System.out.println("Logging RANDOM time");
             nTimes = times;
