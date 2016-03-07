f6a9a830fc4abd79afc25a4466a31c0b184a223c
Carla <carla@pitando.com>
2016-03-07 17:12:42 +0100

    TPP - lab07 first commit

---
+++ TPP/Lab Uploads/i3-lab07/linq/Functions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace TPP.Laboratory.Functional.Lab07 {
+
+    static public class Functions {
+
+        public static IEnumerable<TResult> Map<TElement, TResult>(this IEnumerable<TElement> collection, Func<TElement, TResult> function) {
+            TResult[] result = new TResult[collection.Count()];
+            int i = 0;
+            foreach (TElement x in collection)
+                result[i++] = function(x);
+            return result;
+        }
+
+    }
+}
---
+++ TPP/Lab Uploads/i3-lab07/linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TPP.Laboratory.Functional.Lab07 {
+
+    class Program {
+
+        static void Main() {
+            IEnumerable<int> integers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+    }
+}
