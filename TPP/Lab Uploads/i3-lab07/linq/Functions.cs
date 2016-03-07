0fb02471624ab34c5dd7a55a34540cd97bc95a0d
Carla <carla@pitando.com>
2016-03-07 18:42:44 +0100

    TPP - lazy Map

--- TPP/Lab Uploads/i3-lab07/linq/Functions.cs
+++ TPP/Lab Uploads/i3-lab07/linq/Functions.cs
@@ -7,11 +7,10 @@ namespace TPP.Laboratory.Functional.Lab07 {
     static public class Functions {
 
         public static IEnumerable<TResult> Map<TElement, TResult>(this IEnumerable<TElement> collection, Func<TElement, TResult> function) {
-            TResult[] result = new TResult[collection.Count()];
-            int i = 0;
-            foreach (TElement x in collection)
-                result[i++] = function(x);
-            return result;
+            foreach (TElement x in collection) {
+                yield return function(x);
+            }
+            yield break;
         }
 
     }
--- TPP/Lab Uploads/i3-lab07/linq/Program.cs
+++ TPP/Lab Uploads/i3-lab07/linq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace TPP.Laboratory.Functional.Lab07 {
 
@@ -7,6 +8,14 @@ namespace TPP.Laboratory.Functional.Lab07 {
 
         static void Main() {
             IEnumerable<int> integers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach (int num in (integers.Map(x => x * 2))){
+                Console.WriteLine(num);
+            }
+            integers.
+            
+
+
         }
     }
 }
